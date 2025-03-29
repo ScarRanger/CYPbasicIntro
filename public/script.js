fa55ca27314eeb14721f37ee4d1e319ab8368af4
Rhine Pereira <rhine.pereira@gmail.com>
2025-03-29 15:33:19 +0530

    icebreaker verses

--- public/script.js
+++ public/script.js
@@ -16,4 +16,35 @@ function prevSlide() {
     showSlide(slideIndex);
 }
 
-setInterval(nextSlide, 5000); 
+setInterval(nextSlide, 5000);
+
+
+function icebreakerUnlock() {
+    const targetDateIST = new Date('2025-03-29T15:00:00+05:30');
+    const targetTimeIST = targetDateIST.getTime();
+
+    function checkTimeAndUnlock() {
+        const nowUTC = Date.now();
+        const nowIST = new Date(nowUTC).toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+        const nowISTTimestamp = new Date(nowIST).getTime();
+
+        if (nowISTTimestamp >= targetTimeIST) {
+            const container = document.getElementById('timedButtonContainer');
+            if (container) {
+                container.classList.add('unlocked');
+                container.innerHTML = 'Click Me!';
+                container.onclick = function () {
+                    window.location.href = "verse.html"
+                }
+            }
+        } else {
+            setTimeout(checkTimeAndUnlock, 1000); // Check every second
+        }
+    }
+
+    // Start the time check immediately
+    checkTimeAndUnlock();
+}
+
+// Call icebreakerUnlock() after the page has loaded
+window.onload = icebreakerUnlock;
